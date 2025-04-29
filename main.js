897ff7e1295cb989cb8d4e12a46cd086e6717520
Akshit <akshitmathur2003@gmail.com>
2025-04-29 20:03:13 +0530

    final

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 console.log("Main.js working")
-const populate = async(value, currency) => {
+const populate = async (value, currency) => {
     url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_odavVxTT5trYQ54cllrUUsPTnc0NHLFsEQacqW0e&base_currency=" + currency
+    let myStr = ""
     let response = await fetch(url)
     let rJson = await response.json()
     console.log(rJson)
-    for(let key in Object.keys(rJson["data"])){
-        key,rJson["data"][key]["code"],rJson["data"][key]["value"]
+    for (let key of Object.keys(rJson["data"])) {
+        myStr += `<tr>
+                    <td>${key}</td>
+                    <td>${rJson["data"][key]["code"]}</td>
+                    <td>${rJson["data"][key]["value"] * value}</td>
+                </tr>
+                `
     }
     const tableBody = document.querySelector("tbody")
-    tableBody.innerHTML = 
-    <tr>
-    <td>Data1</td>
-    <td>Data2</td>
-    <td>Data3</td>
-    </tr>
-    `
+    tableBody.innerHTML = myStr;
 }
 const btn = document.querySelector('.btn')
 btn.addEventListener("click", (e) => {
